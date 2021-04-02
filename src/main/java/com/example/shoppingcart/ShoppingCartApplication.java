1996cd2953cec79bd72bd45475e9d4d9d00c5f2d
arman <arman.ajabkhani@snapp.cab>
2021-04-03 00:57:28 +0430

    Create base structure for shopping cart as a springboot project
    Application can run currently.
    These technology/framework will use and config
     - Spring boot
     - Spring mvc
     - Undertow
     - Maven
     - JUnit
     - Lombok

---
+++ src/main/java/com/example/shoppingcart/ShoppingCartApplication.java
@@ -0,0 +1,57 @@
+package com.example.shoppingcart;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@Slf4j
+public class ShoppingCartApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext =
+                SpringApplication.run(ShoppingCartApplication.class, args);
+      logApplicationStartup(applicationContext);
+    }
+
+    private static void logApplicationStartup( ConfigurableApplicationContext context) {
+		Environment env = context.getEnvironment();
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (!StringUtils.hasText(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}'  is running! \t\n Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
+}
